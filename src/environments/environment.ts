9a998d954045c7444b9b6e05fe45e4bec33981c9
Hegar Garcia <hegargarcia@gmail.com>
2019-05-22 17:16:01 -0500

    Adding firebase credentials

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDutx-DRweXdduYZajtuqvOc9JFhJT0hC0',
+    authDomain: 'snet2019.firebaseapp.com',
+    databaseURL: 'https://snet2019.firebaseio.com',
+    projectId: 'snet2019',
+    storageBucket: 'snet2019.appspot.com',
+    messagingSenderId: '544744873113',
+    appId: '1:544744873113:web:d216bf0a26393110'
+  }
 };
 
 /*
