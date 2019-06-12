786b288bddd32336d287fc9b3b79685c0e57d36f
Hegar Garcia <hegargarcia@gmail.com>
2019-06-12 08:19:10 -0500

    Move post creator from textarea to input

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,8 +1,5 @@
-import { Component, OnInit, NgZone, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
-import { CdkTextareaAutosize } from '@angular/cdk/text-field';
-
-import { take } from 'rxjs/operators';
 
 import { PostsService, IPost } from '@core/posts/posts.service';
 
@@ -12,23 +9,16 @@ import { PostsService, IPost } from '@core/posts/posts.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  @ViewChild('autosize', { static: false }) autosize: CdkTextareaAutosize;
   public posts: IPost[];
   public contentForm = new FormControl('', [
     Validators.minLength(1),
     Validators.maxLength(100)
   ]);
 
-  constructor(private postsService: PostsService, private ngZone: NgZone) {}
+  constructor(private postsService: PostsService) {}
 
   ngOnInit() {
     this.postsService.getTimeline().subscribe(data => (this.posts = data));
-  }
-
-  triggerResize() {
-    this.ngZone.onStable
-      .pipe(take(1))
-      .subscribe(() => this.autosize.resizeToFitContent(true));
   }
 
   submit() {
